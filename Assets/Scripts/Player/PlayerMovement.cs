9c2086372d9f06049fc7d1362a2a4f42b733a8a3
CyanRa <sashazay99@gmail.com>
2026-01-02 16:38:33 +0100

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using System;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Component References")]
+    [SerializeField] Rigidbody2D rb;
+    
+    [Header("Player Settings")]
+    [SerializeField]float speed;
+
+    float horizontal;
+    float vertical;
+
+
+    #region PLAYER_CONTROLS
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal*speed, vertical*speed);
+        
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+        vertical = context.ReadValue<Vector2>().y;
+    }
+    #endregion
+}
